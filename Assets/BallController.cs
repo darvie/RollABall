397b38453b352ff25ca882c68b5368187d7a5609
darvie <ddhill01@student.ubc.ca>
2025-01-21 10:54:47 -0800

    Refactored Input Handling

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -2,47 +2,13 @@ using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    //Unity allows for private variables to be exposed in the inspector by simply adding a [SerializeField] attribute
     
     [SerializeField] private Rigidbody sphereRigidbody;
-    [SerializeField] private float ballSpeed = 2f;
-
-
-    void Start()
+    [SerializeField] float ballSpeed;
+    public void MoveBall(Vector2 input)
     {
-        Debug.Log("Calling the Start method");
+         Vector3 inputXZPlane = new(input.x, 0, input.y);
+         sphereRigidbody.AddForce(inputXZPlane * ballSpeed);
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        Vector2 inputVector = Vector2.zero; // intialize our input vector
-        if (Input.GetKey(KeyCode.W))
-            {
-                Debug.Log(Vector2.up);
-            inputVector += Vector2.up; // "a += b" <=> "a = a + b"
-
-        }
-        if (Input.GetKey(KeyCode.A))
-        {
-            Debug.Log(Vector2.left);
-            inputVector += Vector2.left;
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            Debug.Log(Vector2.down);
-            inputVector += Vector2.down;
-        }
-        if (Input.GetKey(KeyCode.D))
-        {
-            Debug.Log(Vector2.right);
-            inputVector += Vector2.right;
-        }
-
-        Vector3 inputXZPlane = new Vector3(inputVector.x, 0, inputVector.y);
-        sphereRigidbody.AddForce(inputXZPlane * ballSpeed);
-
-
-    }
 }

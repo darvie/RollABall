2ccdaec65785bf8519066aa014018f478adafd66
darvie <ddhill01@student.ubc.ca>
2025-01-28 23:11:04 -0800

    Added Jump Functionality

--- Assets/BallController.cs
+++ Assets/BallController.cs
@@ -1,10 +1,16 @@
+using System;
 using UnityEngine;
 
 public class BallController : MonoBehaviour
 {
-    
     [SerializeField] private Rigidbody sphereRigidbody;
     [SerializeField] float ballSpeed;
+    
+    public void Start()
+    {
+        Debug.Log("Calling the Start method");
+
+    }
     public void MoveBall(Vector2 input)
     {
          Vector3 inputXZPlane = new(input.x, 0, input.y);
--- Assets/InputManager.cs
+++ Assets/InputManager.cs
@@ -1,31 +1,47 @@
+using System;
 using UnityEngine;
 using UnityEngine.Events;
 
 public class InputManager : MonoBehaviour
 {
+    [SerializeField] private Boolean Grounded = true;
+    [SerializeField] private float jumpForce = 2.0f;
+    [SerializeField] private Rigidbody sphereRigidbody;
 
     public UnityEvent<Vector2> OnMove = new UnityEvent<Vector2>();
     void Update()
     {
-        Vector2 inputVector = Vector2.zero;
+        Vector3 inputVector = Vector3.zero;
          if (Input.GetKey(KeyCode.W))
              {
-             inputVector += Vector2.up;
+             inputVector += Vector3.up;
              }
          if (Input.GetKey(KeyCode.S))
              {
-             inputVector += Vector2.down;
+             inputVector += Vector3.down;
              }
          if (Input.GetKey(KeyCode.A))
              {
-             inputVector += Vector2.left;
+             inputVector += Vector3.left;
              }
          if (Input.GetKey(KeyCode.D))
              {
-             inputVector += Vector2.right;
-             }
+             inputVector += Vector3.right;
+        }
 
+        if (Input.GetKeyDown(KeyCode.Space) && Grounded)
+        {
+            sphereRigidbody.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            Grounded = false;
+        }
         OnMove?.Invoke(inputVector);
 
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            Grounded = true;
+        }
     }
 }
